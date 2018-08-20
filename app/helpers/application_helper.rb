1634c641a552bbdeb4129720d95b565fb37c5651
Fabio Soares <fabio.soares@autoseg.com>
2018-08-20 19:54:06 -0300

    Adapting views to access mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,45 +1,30 @@
 module ApplicationHelper
   def menu
-    items = [home, alagamento, desmatamento, mananciais, poluicao, temperatura, transito, sobre, informativo]
-    content_tag :ul, :class => "nav" do
+    items = [inicio, temperatura, poluicao, transito, sobre]
+    content_tag :ul, :class => "navpils navbar-nav navbar-center" do
+    #content_tag :ul, :class => "nav nav-tabs nav-justified" do
       items.collect { |item| concat item}
     end
   end
 
-  def home
-    link_to "Início \n", root_path
-  end
-
-  def alagamento
-    link_to "Alagamentos \n", alagamentos_index_path
-  end
-
-  def desmatamento
-    link_to "Desmatamento \n", desmatamento_index_path
-  end
-
-  def mananciais
-    link_to "Mananciais \n", mananciais_index_path
+  def inicio
+    link_to "Home |", poluicao_index_path
   end
 
   def poluicao
-    link_to "Poluição \n", poluicao_index_path
+    link_to "Poluição |", poluicao_index_path
   end
 
   def temperatura
-    link_to "Temperatura \n", temperatura_index_path
+    link_to "Temperatura |", temperatura_index_path
   end
 
   def transito
-    link_to "Trânsito \n", transito_index_path
+    link_to "Trânsito |", transito_index_path
   end
 
   def sobre
-    link_to "Sobre \n", sobre_index_path
-  end
-
-  def informativo
-    link_to "Informativo \n", informativos_url
+    link_to "Sobre", sobre_index_path
   end
 
   def buttons
@@ -50,6 +35,6 @@ module ApplicationHelper
   end
 
     def voltar
-      link_to "Voltar \n", root_path
+      link_to "Voltar |", root_path
     end
 end
